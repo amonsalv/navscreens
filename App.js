5a2b05b8d6b4ba3998bf81a40009ce623f8e65e8
madnesal <madnesalio@gmail.com>
2023-04-20 21:08:31 -0500

    Creacion del navegardo

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
-import { Text, View, Button } from 'react-native';
-//Librerias para el manejo de navegacion entre pantallas
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button} from 'react-native-paper';
+// Importar componentes para la navegación y generación de la pila de screens
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons} from '@expo/vector-icons';
+
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import Contacts from './screens/Contacts';
 import Login from './screens/Login';
 
+// Crear constante para generar las rutas de los screens
+let users = [
+  {username:'hruiz',name:'Humberto Ruiz',password:'11', role:1},
+  {username:'jdoe',name:'John Doe',password:'22', role:2}
+]
+
 const Stack = createNativeStackNavigator(); //Definir las rutas de cada componente
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
 
-        <Stack.Screen name="Home" component={HomeScreen} options={{title:'Inicio'}} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{title:'Login'}} />
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{title:'Inicio'}} />
+
         <Stack.Screen name="Profile" component={ProfileScreen} options={{title:'Perfil del Usuario'}}/>
         <Stack.Screen name="Contacts" component={Contacts} options={{title:'Contactenos'}}/>
 
       </Stack.Navigator>
     </NavigationContainer>
   );
+  //        <Stack.Screen name="Login" component={LoginScreen} options={{title:'Login'}} />
 }
+
+//function ProfileScreen
+
+function HomeScreen({navigation}){
+  return(
+    <View style>
+
+    </View>
+  )
+}
+
+function HomeTabs(){ //agregamos la barra de navegacion 
+  return(
+    <Tab.Navigator
+    screenOptions={{
+      headerShown:false,
+      tabBarActiveTintColor:'red',
+      tabBarInactiveTintColor:'gray',
+      tabBarActiveBackgroundColor:'aqua',
+      tabBarInactiveBackgroundColor:'powderblue'
+    }}
+    >
+      <Tab.Screen name= "Home" component={HomeScreen} options={{
+        tabBarStyle:{display:none}// que el log in tenga desactivado elboton tabs mientras ingresa el usuario
+        tabBarIcon: (tabInfo) => ()
+      }} />
+      <Tab.Screen name= "Profile" component={HomeScreen} options={{}}/>
+      <Tab.Screen name= "Contacts" component={HomeScreen} options={{}}/>
+    </Tab.Navigator>
+  );
+}
+
--- screens/Contacts.jsx
+++ screens/Contacts.jsx
@@ -2,7 +2,6 @@ import { View, Text, Button } from "react-native";
 import { styles } from '../assets/styles/styles';
 
 export default function Contacts(){
-  const {email, phone} = route.params
     return(
       <View style={styles.container}>
         <Text style={{fontWeight: 'bold'}}>Estamos en Contactos</Text>
